42c49dac924c66c15f9312011704a00a4232676d
FelipeMolinaa <felipemolina.contato@gmail.com>
2023-08-24 22:29:57 -0300

    [feat] adição do projeto angular com angular-universal

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
