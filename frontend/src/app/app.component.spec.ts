3281d09a93e185d18ff71b295f396b9800b4941b
FelipeMolinaa <felipemolina.contato@gmail.com>
2023-08-24 22:52:49 -0300

    [feat] Adição primeNg e tailwind

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pontuacaoWeb app is running!');
+    expect(compiled.querySelector('h2')?.textContent).toContain('Hello World');
   });
 });
