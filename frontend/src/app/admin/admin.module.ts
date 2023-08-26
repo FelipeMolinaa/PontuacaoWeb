42d1eec9aac0f472d59ad82a86f281cad8def724
FelipeMolinaa <felipemolina.contato@gmail.com>
2023-08-26 04:02:24 +0000

    [feat] Reconfiguradas rotas

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule],
-    declarations: [AdminComponent],
+    declarations: [AdminComponent, LoginComponent],
 })
-export class AdminModule {}
+export class AdminModule { }
