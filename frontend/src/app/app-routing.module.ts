e001f5e3c3c1296353a6327d7f8d252a764a0ab4
FelipeMolinaa <felipemolina.contato@gmail.com>
2023-08-26 03:58:38 +0000

    [fix] correções de bibliotecas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'admin', pathMatch: 'full' },
-    { path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule) },
+    // { path: '', redirectTo: 'admin', pathMatch: 'full' },
+    // { path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule) },
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,7 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
-    providers: [provideClientHydration()],
+    providers: [],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
